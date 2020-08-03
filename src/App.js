81c093bc8f2754c8f1163aef66c034749dc89326
Matthew Henry <tadaemusic@gmail.com>
2020-08-03 18:38:22 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
+    <div className="app">
+      {/* Header */}
+      <div className="app__header">
+        <img
+          className="app__headerImage" 
+          src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
+          alt=""
+          />
+      </div>
+          
+    
+    
+    
 
-
-        <div className="app__headerImage">
-          <img>
-
-          </img>
-        </div>
-        {/* Header */}
-
-        {/* POST */}
-        {/* POST */}
-        {/* POST */}
-      </header>
+    {/* POST */}
+    {/* POST */}
+    {/* POST */}
     </div>
+
   );
 }
 
---
+++ src/components/js/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <h3>Username</h3>
+            {/* header -> avatar + username */}
+            {/* image */}
+            {/* username + caption */}
+        </div>
+    )
+}
+
+export default Post
