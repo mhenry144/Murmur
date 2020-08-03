8edd690a37d7a4d2e7f34832ada03b9e3b0d56f5
Matthew Henry <tadaemusic@gmail.com>
2020-08-03 16:43:03 -0400

    app css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
+
+
+        <div className="app__headerImage">
+          <img>
+
+          </img>
+        </div>
+        {/* Header */}
+
+        {/* POST */}
+        {/* POST */}
+        {/* POST */}
       </header>
     </div>
   );
