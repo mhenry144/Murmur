c3a8aa4c9c9249d8a6b86a4898c5fba345d3e9c2
Matthew Henry <tadaemusic@gmail.com>
2020-08-04 19:39:54 -0400

    post.js updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import React from 'react';
-import './App.css';
-import Post from './components/js/Post.js';
+import React, { useState } from "react";
+import "./App.css";
+import Post from "./components/js/Post.js";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username:"Matt",
+      caption: "What a nice day!",
+      imageUrl: "https://images.unsplash.com/photo-1506147854445-5a3f534191f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+    },
+    {
+      username: "Dilshan",
+      caption: "WOW",
+      imageUrl: "https://images.unsplash.com/photo-1567105339488-fbb61ca19b36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+    },
+    { 
+      username: "Thomas",
+      caption: "Sunset",
+      imageUrl: "https://images.unsplash.com/photo-1594702296874-1fe2c8d2c731?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
+    },
+    { 
+      username: "Atticus",
+      caption: "I hate the desert!",
+      imageUrl: "https://images.unsplash.com/photo-1596374246413-caaca268d445?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" 
+    }
+
+  ]);
+
   return (
     <div className="app">
       {/* Header */}
+
       <div className="app__header">
+        <h2>Murmur</h2>
         <img
-          className="app__headerImage" 
+          className="app__headerImage"
           src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
           alt=""
-          />
+        />
       </div>
-          
-    
-    
-    
-    <Post />
-    <Post />
-    <Post />
-    {/* POST */}
-    {/* POST */}
-    {/* POST */}
-    </div>
 
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+      
+      {/* POSTS */}
+
+    </div>
   );
 }
 
--- src/components/js/Post.js
+++ src/components/js/Post.js
@@ -2,28 +2,28 @@ import React from "react";
 import "../css/Post.css";
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
     <div className="post">
       {/* header -> avatar + username */}
       <div className="post__header">
         <Avatar
           className="post__avatar"
-          alt=""
+          alt={username}
           src="/static/images/avatar/1.jpg"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
       {/* image */}
       <img
         className="post__image"
-        src="https://images.unsplash.com/photo-1506147854445-5a3f534191f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        src={imageUrl}
         alt=""
       />
 
       {/* username + caption */}
       <h4 className="post__text">
-        <strong>Username</strong> Wow What a day!
+        <strong>{username}</strong> {caption}
       </h4>
     </div>
   );
