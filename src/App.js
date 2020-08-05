44ba06350d407d5e3dea5a4b889154de6c365d44
Matthew Henry <tadaemusic@gmail.com>
2020-08-04 20:57:30 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import Post from "./components/js/Post.js";
 function App() {
   const [posts, setPosts] = useState([
     {
-      username:"Matt",
+      username: "Matt",
       caption: "What a nice day!",
-      imageUrl: "https://images.unsplash.com/photo-1506147854445-5a3f534191f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+      imageUrl:
+        "https://images.unsplash.com/photo-1506147854445-5a3f534191f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
     },
     {
       username: "Dilshan",
       caption: "WOW",
-      imageUrl: "https://images.unsplash.com/photo-1567105339488-fbb61ca19b36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+      imageUrl:
+        "https://images.unsplash.com/photo-1567105339488-fbb61ca19b36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
     },
-    { 
+    {
       username: "Thomas",
       caption: "Sunset",
-      imageUrl: "https://images.unsplash.com/photo-1594702296874-1fe2c8d2c731?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
+      imageUrl:
+        "https://images.unsplash.com/photo-1594702296874-1fe2c8d2c731?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
     },
-    { 
+    {
       username: "Atticus",
       caption: "I hate the desert!",
-      imageUrl: "https://images.unsplash.com/photo-1596374246413-caaca268d445?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" 
-    }
-
+      imageUrl:
+        "https://images.unsplash.com/photo-1596374246413-caaca268d445?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    },
   ]);
 
   return (
@@ -39,15 +42,15 @@ function App() {
           alt=""
         />
       </div>
-
-      {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
-        ))
-      }
-      
       {/* POSTS */}
 
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
